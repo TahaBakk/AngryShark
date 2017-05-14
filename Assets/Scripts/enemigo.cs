f4cc41a5c370e8ef25ac086c943d8caaea06a597
TahaBakk <tahabakk2@gmail.com>
2017-05-14 19:00:20 +0200

    Echa la parte de logica

---
+++ Assets/Scripts/enemigo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigo : MonoBehaviour {
+
+	public GameObject efectoEnemigo;
+
+	public float health = 4f;
+
+	public static int vidas = 0;
+	public GameObject proximo;
+
+
+ 	void Start() {
+		vidas++;
+    }
+
+	void OnCollisionEnter2D (Collision2D colInfo)
+	{
+		if (colInfo.relativeVelocity.magnitude > health)
+		{
+			muere();
+		}
+	}
+
+	void muere ()
+	{
+		Instantiate(efectoEnemigo, transform.position, Quaternion.identity);
+		vidas--;
+		if(vidas<=0)
+		{//Controlar el fin de partida
+			Debug.Log("Gola partida finalizada");
+		}
+		Destroy(gameObject);
+
+	}
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour {
+
+	public Rigidbody2D Jugador;
+	public Rigidbody2D lanzar;
+
+	//public Rigidbody2D gancho;
+
+	public float soltar = .15f;
+	public float distanciaMaxArrastre = 4f;
+
+	public GameObject proximoObjeto;
+
+	private bool presionada = false;
+
+	void Update ()
+	{
+		if (presionada)
+		{
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			if(Vector3.Distance(mousePosition,lanzar.position) > distanciaMaxArrastre)
+			{
+				Jugador.position = lanzar.position + (mousePosition - lanzar.position).normalized * distanciaMaxArrastre;
+			}else
+			{
+				Jugador.position = mousePosition;
+			}
+
+		}
+	}
+
+
+	void OnMouseDown ()
+	{
+		presionada = true;
+		Jugador.isKinematic = true;
+	}
+
+	void OnMouseUp ()
+	{
+		presionada = false;
+		Jugador.isKinematic = false;
+
+		StartCoroutine(Release());
+	}
+
+	IEnumerator Release ()
+	{
+		yield return new WaitForSeconds(soltar);
+
+		GetComponent<SpringJoint2D>().enabled = false;
+		this.enabled = false;
+
+		yield return new WaitForSeconds(2f);
+
+		if(proximoObjeto != null)
+		{
+			proximoObjeto.SetActive(true);
+		} else
+		{
+			enemigo.vidas = 0;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
